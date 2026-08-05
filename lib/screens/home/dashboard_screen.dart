889fa9a8cbcc980d0e1ceba0beb0abd3927ba037
Bibek Giri <giribibek01@gmail.com>
2026-08-05 19:33:48 +0545

    Refactor DashBoardScreen and implement BottomNavigationBar with icons for game roles

--- lib/screens/home/dashboard_screen.dart
+++ lib/screens/home/dashboard_screen.dart
@@ -1,15 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:valorant_guide_app/screens/home/home_screen.dart';
 
-class DashboardScreen extends StatefulWidget {
-  const DashboardScreen({super.key});
+class DashBoardScreen extends StatefulWidget {
+  const DashBoardScreen({super.key});
 
   @override
-  State<DashboardScreen> createState() => _DashboardScreenState();
+  State<DashBoardScreen> createState() => _DashBoardScreenState();
 }
 
-class _DashboardScreenState extends State<DashboardScreen> {
+class _DashBoardScreenState extends State<DashBoardScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _pages = [
+    Center(child: Text("Controller")),
+    Center(child: Text("Duelist")),
+    HomeScreen(),
+    Center(child: Text("Initiator")),
+    Center(child: Text("Sentinel")),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _pages[_currentIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xFFFF4654),
+        // showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Color(0xFF1C252E),
+        unselectedItemColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+        },
+
+        items: [
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/logo/controller_logo.png"),
+              size: 28,
+            ),
+            label: "Controller",
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/logo/duelist_logo.png"),
+              size: 28,
+            ),
+            label: "Duelist",
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/logo/valorant_logo.png"),
+              size: 28,
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/logo/initiator_logo.png"),
+              size: 28,
+            ),
+            label: "Initiator",
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/logo/sentinel_logo.png"),
+              size: 28,
+            ),
+            label: "Sentinel",
+          ),
+        ],
+      ),
+    );
   }
 }
