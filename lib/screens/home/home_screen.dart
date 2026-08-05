121a9fa02dad2603fc627b2121364923cec6b123
Bibek Giri <giribibek01@gmail.com>
2026-08-05 19:12:40 +0545

    Enhance HomeScreen layout and add DashboardScreen placeholder

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFF1C252E),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircleAvatar(
+                  radius: 20,
+                  backgroundColor: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 2,
+                      vertical: 12,
+                    ),
+                    child: Image.asset(
+                      "assets/logo/valorant_logo.png",
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+
+                CircleAvatar(radius: 20),
+              ],
+            ),
+
+            SizedBox(height: 20),
+
+            Text(
+              "Learn Your\nFavorite Agents",
+              style: GoogleFonts.orbitron(fontSize: 30, color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
