188b4fce27867afe019f73cd07cc5fbd542a910b
Bibek Giri <giribibek01@gmail.com>
2026-08-06 17:53:25 +0545

    Update main.dart to set HomeScreen as the main entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:valorant_guide_app/screens/agents/agents_detail_screen.dart';
+import 'package:valorant_guide_app/screens/home/home_screen.dart';
 import 'package:valorant_guide_app/screens/home/splash_screen.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const AgentsDetailScreen(),
+      home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
