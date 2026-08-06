97827c7bd7447160eaf63a7b40b2e53a4e51fefa
Bibek Giri <giribibek01@gmail.com>
2026-08-06 13:25:09 +0545

    Add abilityIcons list to store agent ability asset paths

---
+++ lib/data/agent_data.dart
@@ -0,0 +1,6 @@
+final List<String> abilityIcons = [
+  'assets/abilities/paranoia.webp',
+  'assets/abilities/dark_cover.webp',
+  'assets/abilities/shrouded_step.webp',
+  'assets/abilities/from_the_shadows.webp',
+];
