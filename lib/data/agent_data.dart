2a4146f9a0e4d4f2da3403ec6e56d69c9cc378ed
Bibek Giri <giribibek01@gmail.com>
2026-08-07 21:22:40 +0545

    Updated the Data for Agents

--- lib/data/agent_data.dart
+++ lib/data/agent_data.dart
@@ -17,42 +17,6 @@ List duelistsData = [
     "image": "assets/duelists/Phoenix.webp",
     "roleIcon": "assets/logo/duelist_logo.png",
   },
-  {
-    "name": "Reyna",
-    "role": "Duelist",
-    "image": "assets/duelists/Reyna.webp",
-    "roleIcon": "assets/logo/duelist_logo.png",
-  },
-  {
-    "name": "Yoru",
-    "role": "Duelist",
-    "image": "assets/duelists/Yoru.webp",
-    "roleIcon": "assets/logo/duelist_logo.png",
-  },
-  {
-    "name": "Neon",
-    "role": "Duelist",
-    "image": "assets/duelists/Neon.webp",
-    "roleIcon": "assets/logo/duelist_logo.png",
-  },
-  {
-    "name": "Iso",
-    "role": "Duelist",
-    "image": "assets/duelists/Iso.webp",
-    "roleIcon": "assets/logo/duelist_logo.png",
-  },
-  {
-    "name": "Waylay",
-    "role": "Duelist",
-    "image": "assets/duelists/Waylay.webp",
-    "roleIcon": "assets/logo/duelist_logo.png",
-  },
-  {
-    "name": "Raze",
-    "role": "Duelist",
-    "image": "assets/duelists/Raze.webp",
-    "roleIcon": "assets/logo/duelist_logo.png",
-  },
 ];
 
 List controllersData = [
@@ -68,36 +32,6 @@ List controllersData = [
     "image": "assets/controllers/Viper.webp",
     "roleIcon": "assets/logo/controller_logo.png",
   },
-  {
-    "name": "Miks",
-    "role": "Controller",
-    "image": "assets/controllers/Miks.webp",
-    "roleIcon": "assets/logo/controller_logo.png",
-  },
-  {
-    "name": "Clove",
-    "role": "Controller",
-    "image": "assets/controllers/Clove.webp",
-    "roleIcon": "assets/logo/controller_logo.png",
-  },
-  {
-    "name": "Brimstone",
-    "role": "Controller",
-    "image": "assets/controllers/Brimstone.webp",
-    "roleIcon": "assets/logo/controller_logo.png",
-  },
-  {
-    "name": "Harbor",
-    "role": "Controller",
-    "image": "assets/controllers/Harbor.webp",
-    "roleIcon": "assets/logo/controller_logo.png",
-  },
-  {
-    "name": "Astra",
-    "role": "Controller",
-    "image": "assets/controllers/Astra.webp",
-    "roleIcon": "assets/logo/controller_logo.png",
-  },
 ];
 
 List initiatorsData = [
@@ -111,36 +45,6 @@ List initiatorsData = [
     "name": "Fade",
     "role": "Initiator",
     "image": "assets/initiators/Fade.webp",
-    "roleIcon": "assets/logo/initiator_logo.png",
-  },
-  {
-    "name": "Gekko",
-    "role": "Initiator",
-    "image": "assets/initiators/Gekko.webp",
-    "roleIcon": "assets/logo/initiator_logo.png",
-  },
-  {
-    "name": "Kayo",
-    "role": "Initiator",
-    "image": "assets/initiators/Kayo.webp",
-    "roleIcon": "assets/logo/initiator_logo.png",
-  },
-  {
-    "name": "Skye",
-    "role": "Initiator",
-    "image": "assets/initiators/Skye.webp",
-    "roleIcon": "assets/logo/initiator_logo.png",
-  },
-  {
-    "name": "Sova",
-    "role": "Initiator",
-    "image": "assets/initiators/Sova.webp",
-    "roleIcon": "assets/logo/initiator_logo.png",
-  },
-  {
-    "name": "Tejo",
-    "role": "Initiator",
-    "image": "assets/initiators/Tejo.webp",
     "roleIcon": "assets/logo/initiator_logo.png",
   },
 ];
