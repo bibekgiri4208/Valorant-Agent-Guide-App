fff40ea603f4f99e23cd92fc42a1675f2202a3d8
Bibek Giri <giribibek01@gmail.com>
2026-08-07 09:14:28 +0545

    Add Controllers and Initiators data with agent details and images

--- lib/data/agent_data.dart
+++ lib/data/agent_data.dart
@@ -54,3 +54,93 @@ List duelistsData = [
     "roleIcon": "assets/logo/duelist_logo.png",
   },
 ];
+
+List controllersData = [
+  {
+    "name": "Omen",
+    "role": "Controller",
+    "image": "assets/controllers/Omen.webp",
+    "roleIcon": "assets/logo/controller_logo.png",
+  },
+  {
+    "name": "Viper",
+    "role": "Controller",
+    "image": "assets/controllers/Viper.webp",
+    "roleIcon": "assets/logo/controller_logo.png",
+  },
+  {
+    "name": "Miks",
+    "role": "Controller",
+    "image": "assets/controllers/Miks.webp",
+    "roleIcon": "assets/logo/controller_logo.png",
+  },
+  {
+    "name": "Clove",
+    "role": "Controller",
+    "image": "assets/controllers/Clove.webp",
+    "roleIcon": "assets/logo/controller_logo.png",
+  },
+  {
+    "name": "Brimstone",
+    "role": "Controller",
+    "image": "assets/controllers/Brimstone.webp",
+    "roleIcon": "assets/logo/controller_logo.png",
+  },
+  {
+    "name": "Harbor",
+    "role": "Controller",
+    "image": "assets/controllers/Harbor.webp",
+    "roleIcon": "assets/logo/controller_logo.png",
+  },
+  {
+    "name": "Astra",
+    "role": "Controller",
+    "image": "assets/controllers/Astra.webp",
+    "roleIcon": "assets/logo/controller_logo.png",
+  },
+];
+
+List initiatorsData = [
+  {
+    "name": "Breach",
+    "role": "Initiator",
+    "image": "assets/initiators/Breach.webp",
+    "roleIcon": "assets/logo/initiator_logo.png",
+  },
+  {
+    "name": "Fade",
+    "role": "Initiator",
+    "image": "assets/initiators/Fade.webp",
+    "roleIcon": "assets/logo/initiator_logo.png",
+  },
+  {
+    "name": "Gekko",
+    "role": "Initiator",
+    "image": "assets/initiators/Gekko.webp",
+    "roleIcon": "assets/logo/initiator_logo.png",
+  },
+  {
+    "name": "Kayo",
+    "role": "Initiator",
+    "image": "assets/initiators/Kayo.webp",
+    "roleIcon": "assets/logo/initiator_logo.png",
+  },
+  {
+    "name": "Skye",
+    "role": "Initiator",
+    "image": "assets/initiators/Skye.webp",
+    "roleIcon": "assets/logo/initiator_logo.png",
+  },
+  {
+    "name": "Sova",
+    "role": "Initiator",
+    "image": "assets/initiators/Sova.webp",
+    "roleIcon": "assets/logo/initiator_logo.png",
+  },
+  {
+    "name": "Tejo",
+    "role": "Initiator",
+    "image": "assets/initiators/Tejo.webp",
+    "roleIcon": "assets/logo/initiator_logo.png",
+  },
+];
