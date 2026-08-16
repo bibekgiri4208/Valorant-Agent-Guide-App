85f70fc3fd2a46e6d686c54cb975b9c9ce0892ac
Bibek Giri <giribibek01@gmail.com>
2026-08-16 18:10:06 +0545

    Add ability assets and data for Chamber and Cypher; update sentinels_data.dart

--- lib/data/sentinels_data.dart
+++ lib/data/sentinels_data.dart
@@ -4,41 +4,81 @@ List sentinelsData = [
     "role": "Sentinel",
     "image": "assets/sentinels/Chamber.webp",
     "roleIcon": "assets/logo/sentinel_logo.png",
+    "country": "France",
+    "agentNumber": "17",
+    "summary":
+        "Well-dressed and well-armed, French weapons designer Chamber expels aggressors with deadly precision. He leverages his custom arsenal to hold the line and pick off enemies from afar, with a contingency built for every plan.",
+    "abilities": [
+      {
+        "name": "Trademark",
+        "icon": "assets/abilities/chamber/trademark.webp",
+        "description":
+            'EQUIP a trap that scans for enemies. FIRE to place it on the ground. When a visible enemy comes in range, the trap counts down and then destabilizes the terrain around them, creating a lingering field that Slows players caught inside of it. The trap can be picked up to be REDEPLOYED.',
+        "video": "assets/abilities/chamber/trademark.mp4",
+      },
+      {
+        "name": "Headhunter",
+        "icon": "assets/abilities/chamber/headhunter.webp",
+        "description":
+            'ACTIVATE to equip a heavy pistol. ALT FIRE with the pistol equipped to aim down sights.',
+        "video": "assets/abilities/chamber/headhunter.mp4",
+      },
+      {
+        "name": "Rendezvous",
+        "icon": "assets/abilities/chamber/rendezvous.webp",
+        "description":
+            'EQUIP a teleport anchor. FIRE to place it on the ground. While on the ground and in range of the anchor, REACTIVATE to quickly teleport to the anchor. The anchor can be picked up to be REDEPLOYED.',
+        "video": "assets/abilities/chamber/rendezvous.mp4",
+      },
+      {
+        "name": "Tour De Force",
+        "icon": "assets/abilities/chamber/tour_de_force.webp",
+        "description":
+            'ACTIVATE to summon a powerful, custom sniper rifle that will kill an enemy with any direct hit to the upper body. ALT FIRE to aim down sights. Killing an enemy creates a lingering field that Slows players caught inside of it.',
+        "video": "assets/abilities/chamber/tour_de_force.mp4",
+      },
+    ],
   },
+
+  //Cypher
   {
     "name": "Cypher",
     "role": "Sentinel",
     "image": "assets/sentinels/Cypher.webp",
     "roleIcon": "assets/logo/sentinel_logo.png",
-  },
-  {
-    "name": "Deadlock",
-    "role": "Sentinel",
-    "image": "assets/sentinels/Deadlock.webp",
-    "roleIcon": "assets/logo/sentinel_logo.png",
-  },
-  {
-    "name": "Killjoy",
-    "role": "Sentinel",
-    "image": "assets/sentinels/Killjoy.webp",
-    "roleIcon": "assets/logo/sentinel_logo.png",
-  },
-  {
-    "name": "Sage",
-    "role": "Sentinel",
-    "image": "assets/sentinels/Sage.webp",
-    "roleIcon": "assets/logo/sentinel_logo.png",
-  },
-  {
-    "name": "Veto",
-    "role": "Sentinel",
-    "image": "assets/sentinels/Veto.webp",
-    "roleIcon": "assets/logo/sentinel_logo.png",
-  },
-  {
-    "name": "Vyse",
-    "role": "Sentinel",
-    "image": "assets/sentinels/Vyse.webp",
-    "roleIcon": "assets/logo/sentinel_logo.png",
+    "country": "Morocco",
+    "agentNumber": "09",
+    "summary":
+        "The Moroccan information broker, Cypher is a one-man surveillance network who keeps tabs on the enemy's every move. No secret is safe. No maneuver goes unseen. Cypher is always watching.",
+    "abilities": [
+      {
+        "name": "Trapwire",
+        "icon": "assets/abilities/cypher/trapwire.webp",
+        "description":
+            'EQUIP a trapwire. FIRE to place a destructible and covert tripwire at the targeted location creating a line that spans between the placed location and the wall opposite. Enemy players who cross a tripwire will be tethered, revealed, and dazed after a short period if they do not destroy the device in time. This ability can be picked up to be REDEPLOYED.',
+        "video": "assets/abilities/cypher/trapwire.mp4",
+      },
+      {
+        "name": "Cyber Cage",
+        "icon": "assets/abilities/cypher/cyber_cage.webp",
+        "description":
+            'INSTANTLY toss the cyber cage in front of Cypher. Activate to create a zone that blocks vision and slows enemies who pass through it.',
+        "video": "assets/abilities/cypher/cyber_cage.mp4",
+      },
+      {
+        "name": "Spycam",
+        "icon": "assets/abilities/cypher/spycam.webp",
+        "description":
+            'EQUIP a spycam. FIRE to place the spycam at the targeted location. RE-USE this ability to take control of the camera\'s view. While in control of the camera, FIRE to shoot a marking dart. This dart will Reveal the location of any player struck by the dart. This ability can be picked up to be REDEPLOYED.',
+        "video": "assets/abilities/cypher/spycam.mp4",
+      },
+      {
+        "name": "Neural Theft",
+        "icon": "assets/abilities/cypher/neural_theft.webp",
+        "description":
+            'INSTANTLY use on a dead enemy player in your crosshairs to reveal the location of all living enemy players.',
+        "video": "assets/abilities/cypher/neural_theft.mp4",
+      },
+    ],
   },
 ];
